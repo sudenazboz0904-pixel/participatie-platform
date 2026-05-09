6fb45a863c4ba11ce8257720b9c7523fea7ff09e
Sudenaz Bozkus <sudenaz@MacBook-Air-van-Sudenaz.local>
2026-05-09 02:29:16 +0200

    static root toegevoegd

--- samen_bepalen/settings.py
+++ samen_bepalen/settings.py
@@ -116,3 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
